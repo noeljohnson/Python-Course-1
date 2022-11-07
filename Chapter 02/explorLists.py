484e564d39641f45f89f582cbe3fbc2695959b6e
Noel Johnson <noel.johnson6213@gmail.com>
2022-11-07 11:18:09 +0530

    Started Chapter 2

---
+++ Chapter 02/explorLists.py
@@ -0,0 +1,11 @@
+##GTG
+
+# Following is a program to explore lists
+
+l = ['a', 1, 2, 'e'] #can store any object
+l1 = [l, 'a'] #also lists are objects
+
+print(l)
+print(l1)
+
+##TYJC
---
+++ Chapter 02/printVowels.py
@@ -0,0 +1,13 @@
+##GTG
+
+# Following program is to print all vowels in a word
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+word = "Milliways"
+
+for letter in word:
+  if (letter in vowels):
+    print(letter)
+
+##TYJC
