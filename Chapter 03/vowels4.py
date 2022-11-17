720817acee69491550c7e7c58a924d8fb07867ec
Noel Johnson <noel.johnson6213@gmail.com>
2022-11-17 11:45:04 +0530

    Continue chapter 3

--- Chapter 03/vowels4.py
+++ Chapter 03/vowels4.py
@@ -1,16 +1,14 @@
-##GTG
-
+## GTG
 inpStr = input("Enter the string to check ")
 vowels = ['a', 'e', 'i', 'o', 'u']
 found = {};
 
-for vowel in vowels:
-  found[vowel] = 0
-
 for letter in inpStr:
   if letter in vowels:
+    found.setdefault(letter, 0)
     found[letter] += 1
-    
+
+
 for k, v in sorted(found.items()):
   print(k, " was found ", v, " times ")
 
