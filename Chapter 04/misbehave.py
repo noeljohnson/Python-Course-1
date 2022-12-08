5276ca2a9db283a9321faf7c55cf13f9f615afb1
Noel Johnson <noel.johnson6213@gmail.com>
2022-12-09 05:20:26 +0530

    Continue chapter 4

---
+++ Chapter 04/misbehave.py
@@ -0,0 +1,13 @@
+##GTG
+
+def double(args):
+  print("Before change", args)
+  args = 2 * args
+  print("After change", args)
+
+def change(args: list):
+  print("Before change", args)
+  args.append("new value")
+  print("After change", args)
+
+##TYJC
---
+++ Chapter 04/myModules/README.txt
@@ -0,0 +1,1 @@
+vsearch is used to find common letters between the two sequences
